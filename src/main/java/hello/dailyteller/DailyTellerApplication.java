fbac8e0f2a79b1010a5d6cc903638b3914f33344
JISUCHOIGIT <wltn203@naver.com>
2025-04-01 21:16:58 +0900

    first commit

---
+++ src/main/java/hello/dailyteller/DailyTellerApplication.java
@@ -0,0 +1,13 @@
+package hello.dailyteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTellerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/dailyteller/DailyTellerApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.dailyteller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyTellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
