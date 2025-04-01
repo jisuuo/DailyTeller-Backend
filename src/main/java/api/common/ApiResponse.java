0b97f81e16b3ce52091e674c70a0a27872273901
JISUCHOIGIT <wltn203@naver.com>
2025-04-01 21:49:37 +0900

    API Response 구현

---
+++ src/main/java/api/common/ApiResponse.java
@@ -0,0 +1,30 @@
+package api.common;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final int status;
+    private final String message;
+    private final T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder().success(true).message("success").data(data).build();
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return ApiResponse.<T>builder().success(true).message(message).data(data).build();
+    }
+
+    public static <T> ApiResponse<T> failure(String message) {
+        return ApiResponse.<T>builder().success(false).message(message).build();
+    }
+
+    public static <T> ApiResponse<T> failure(String message, T data) {
+        return ApiResponse.<T>builder().success(false).message(message).data(data).build();
+    }
+}
