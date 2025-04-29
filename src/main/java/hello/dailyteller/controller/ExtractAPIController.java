9f82ba861515215dda1680335b47131ef37c8601
jissuo <jisu2298@gmail.com>
2025-04-29 21:48:25 +0900

    코드리뷰 수정 완료

--- src/main/java/hello/dailyteller/controller/ExtractAPIController.java
+++ src/main/java/hello/dailyteller/controller/ExtractAPIController.java
@@ -1,12 +1,15 @@
 package hello.dailyteller.controller;
 
+import hello.dailyteller.api.common.ApiResponse;
 import hello.dailyteller.dto.ExtractResponseDto;
 import hello.dailyteller.service.ExtractAPIService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/extractAPI")
 public class ExtractAPIController {
 
     private final ExtractAPIService extractAPIService;
@@ -15,8 +18,13 @@ public class ExtractAPIController {
         this.extractAPIService = extractAPIService;
     }
 
-    @GetMapping("/extractArticle")
-    public ExtractResponseDto getArticleSummary(@RequestParam String url) {
-        return extractAPIService.getArticleSummary(url);
+    @GetMapping("/article")
+    public ApiResponse<ExtractResponseDto> getArticleSummary(@RequestParam String url) {
+        ExtractResponseDto response = extractAPIService.getArticleSummary(url);
+
+        if (response == null) {
+            return ApiResponse.failure("뉴스 추출 실패", response);
+        }
+        return ApiResponse.success(response);
     }
 }
--- src/main/java/hello/dailyteller/service/ExtractAPIService.java
+++ src/main/java/hello/dailyteller/service/ExtractAPIService.java
@@ -1,27 +1,23 @@
 package hello.dailyteller.service;
 
+import hello.dailyteller.api.common.ApiResponse;
 import hello.dailyteller.dto.ExtractRequestDto;
 import hello.dailyteller.dto.ExtractResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
+@RequiredArgsConstructor
 public class ExtractAPIService {
 
-    private final WebClient webClient;
-
-    public ExtractAPIService(@Value("${external.api.base-url}") String baseUrl, @Value("${external.api.key}") String apiKey, @Value("${external.api.host}") String apiHost) {
-        this.webClient = WebClient.builder().baseUrl(baseUrl)
-                        .defaultHeader("X-RapidAPI-Key", apiKey)
-                        .defaultHeader("X-RapidAPI-Host", apiHost)
-                        .build();
-    }
+    private final WebClient extractAPIService;
 
     public ExtractResponseDto getArticleSummary(String url) {
-        ExtractRequestDto extractRequestDto = new ExtractRequestDto(url);
 
-        return webClient.get()
+        return extractAPIService.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/article/proxy/parse") // 또는 /summarize 등 실제 API 경로
                         .queryParam("url", url)
@@ -32,7 +28,8 @@ public class ExtractAPIService {
                         .build())
                 .retrieve()
                 .bodyToMono(ExtractResponseDto.class)
-                .block(); // 동기 호출
+                .block();
+
 
     }
 
