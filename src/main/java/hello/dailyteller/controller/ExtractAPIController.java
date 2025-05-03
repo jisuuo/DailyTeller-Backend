72a910961284db4c0db2020371acb804e128d19d
jissuo <jisu2298@gmail.com>
2025-05-03 23:06:16 +0900

    ResponseEntity 적용

--- src/main/java/hello/dailyteller/controller/ExtractAPIController.java
+++ src/main/java/hello/dailyteller/controller/ExtractAPIController.java
@@ -3,6 +3,7 @@ package hello.dailyteller.controller;
 import hello.dailyteller.api.common.ApiResponse;
 import hello.dailyteller.dto.ExtractResponseDto;
 import hello.dailyteller.service.ExtractAPIService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,12 +20,12 @@ public class ExtractAPIController {
     }
 
     @GetMapping("/article")
-    public ApiResponse<ExtractResponseDto> getArticleSummary(@RequestParam String url) {
+    public ResponseEntity<ApiResponse<ExtractResponseDto>> getArticleSummary(@RequestParam String url) {
         ExtractResponseDto response = extractAPIService.getArticleSummary(url);
 
         if (response == null) {
-            return ApiResponse.failure("뉴스 추출 실패", response);
+            return ResponseEntity.badRequest().body((ApiResponse.failure("뉴스 추출 실패", response)));
         }
-        return ApiResponse.success(response);
+        return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
